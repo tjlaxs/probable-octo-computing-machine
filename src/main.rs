9409dac222118b7f7ef806127cbf76cab2ffbfc6
Tero Laxström <tlax@neonpeons.com>
2025-06-01 12:26:19 +0300

    Adding colors to files, flexboxes still not working

--- src/main.rs
+++ src/main.rs
@@ -1,11 +1,77 @@
 use bevy::prelude::*;
 
-fn init_status(mut commands: Commands, git_status: ResMut<GitStatus>) {
+#[derive(Resource)]
+struct GitStatus(Vec<GitStatusFile>);
+
+struct GitStatusFile(GitStatusFileState, String);
+
+#[derive(Debug, PartialEq, Clone)]
+enum GitStatusFileState {
+    Deleted,
+    Modified,
+    NotTracked,
+    Added,
+    ModifiedInBothStages,
+    AddedThenModified,
+}
+
+impl std::fmt::Display for GitStatusFileState {
+    fn fmt(&self, f: &mut std::fmt::Formatter) -> std::fmt::Result {
+        let mtc = match self {
+            GitStatusFileState::Deleted => " D",
+            GitStatusFileState::Modified => " M",
+            GitStatusFileState::Added => " A",
+            GitStatusFileState::NotTracked => "??",
+            GitStatusFileState::ModifiedInBothStages => "MM",
+            GitStatusFileState::AddedThenModified => "AM",
+        };
+        write!(f, "{}", mtc)
+    }
+}
+
+#[derive(Resource)]
+struct GitStatusTimer(Timer);
+
+pub struct GitStatusPlugin;
+
+impl GitStatusPlugin {}
+
+impl Plugin for GitStatusPlugin {
+    fn build(&self, app: &mut App) {
+        let git_status =
+            " D lorem\n A ipsum\n M dolor\n?? sit\n?? amet\nAM consecteur\nMM adipiscing\n"
+                .to_string();
+        let files = git_status
+            .lines()
+            .map(format_git_status_file)
+            .collect::<Vec<GitStatusFile>>();
+        app.insert_resource(GitStatus(files));
+        app.insert_resource(GitStatusTimer(Timer::from_seconds(
+            5.0,
+            TimerMode::Repeating,
+        )));
+        app.add_systems(Startup, (init_status, show_status));
+    }
+}
+
+fn mod_color(gsf_state: GitStatusFileState) -> TextColor {
+    match gsf_state {
+        GitStatusFileState::Deleted => Color::srgb(230., 0., 0.).into(),
+        GitStatusFileState::Modified => Color::srgb(100., 100., 0.).into(),
+        GitStatusFileState::Added => Color::srgb(0., 0., 200.).into(),
+        GitStatusFileState::NotTracked => Color::srgb(150., 150., 150.).into(),
+        GitStatusFileState::ModifiedInBothStages => Color::srgb(0., 150., 150.).into(),
+        GitStatusFileState::AddedThenModified => Color::srgb(0., 50., 250.).into(),
+    }
+}
+
+fn init_status(mut commands: Commands, status: ResMut<GitStatus>) {
     println!("init_status");
     commands.spawn(Camera2d);
 
+    let font_size = 16.0;
     let text_font = TextFont {
-        font_size: 32.0,
+        font_size,
         ..default()
     };
 
@@ -18,32 +84,28 @@ fn init_status(mut commands: Commands, git_status: ResMut<GitStatus>) {
                 flex_direction: FlexDirection::Column,
                 align_items: AlignItems::Start,
                 justify_items: JustifyItems::Start,
-                padding: UiRect::all(Val::Px(12.)),
-                row_gap: Val::Px(12.),
+                padding: UiRect::all(Val::Px(2.)),
+                row_gap: Val::Px(2.),
                 ..Default::default()
             },
             BackgroundColor(Color::WHITE),
         ))
         .with_children(|builder| {
-            for x in git_status.0.iter() {
+            for x in status.0.iter() {
                 builder
                     .spawn((
                         Node {
-                            width: Val::Percent(15.),
-                            height: Val::Px(40.),
+                            width: Val::Percent(100.),
+                            height: Val::Px(font_size),
                             align_items: AlignItems::Start,
                             justify_items: JustifyItems::Start,
-                            flex_direction: FlexDirection::Row,
                             ..Default::default()
                         },
                         BackgroundColor(Color::linear_rgb(30., 30., 30.)),
                     ))
                     .with_children(|builder| {
-                        builder.spawn((
-                            Text2d::new(x.1.clone()),
-                            text_font.clone(),
-                            TextColor::BLACK,
-                        ));
+                        let color = mod_color(x.0.clone());
+                        builder.spawn((Text2d::new(x.1.clone()), text_font.clone(), color));
                     });
             }
         });
@@ -51,41 +113,18 @@ fn init_status(mut commands: Commands, git_status: ResMut<GitStatus>) {
 
 fn show_status(mut commands: Commands) {}
 
-#[derive(Resource)]
-struct GitStatus(Vec<GitStatusFile>);
-
-struct GitStatusFile(String, String);
-
-#[derive(Resource)]
-struct GitStatusTimer(Timer);
-
-pub struct GitStatusPlugin;
-
-impl GitStatusPlugin {}
-
 fn format_git_status_file(gs_file: &str) -> GitStatusFile {
-    let trimmed = gs_file.trim();
-    let mut split = trimmed.split(' ');
-    let git_type = split.next().unwrap();
-    let git_file = split.next().unwrap();
-    GitStatusFile(git_type.to_string(), git_file.to_string())
-}
-
-impl Plugin for GitStatusPlugin {
-    fn build(&self, app: &mut App) {
-        let git_status =
-            " D LICENSE\n M src/main.rs\n?? README.md\n?? src/awesome.rs\n".to_string();
-        let files = git_status
-            .lines()
-            .map(format_git_status_file)
-            .collect::<Vec<GitStatusFile>>();
-        app.insert_resource(GitStatus(files));
-        app.insert_resource(GitStatusTimer(Timer::from_seconds(
-            5.0,
-            TimerMode::Repeating,
-        )));
-        app.add_systems(Startup, (init_status, show_status));
-    }
+    let (git_status, git_file) = gs_file.split_at(2);
+    let gsfs = match git_status {
+        " D" => GitStatusFileState::Deleted,
+        " M" => GitStatusFileState::Modified,
+        "??" => GitStatusFileState::NotTracked,
+        " A" => GitStatusFileState::Added,
+        "MM" => GitStatusFileState::ModifiedInBothStages,
+        "AM" => GitStatusFileState::AddedThenModified,
+        _ => panic!("Git status file change type not handled"),
+    };
+    GitStatusFile(gsfs, git_file.to_string())
 }
 
 fn main() {
@@ -102,7 +141,7 @@ mod tests {
     #[test]
     fn can_parse_status_file() {
         let result = format_git_status_file("?? src/awesome.rs");
-        assert_eq!(result.0, "??");
+        assert_eq!(result.0.to_string(), "??");
         assert_eq!(result.1, "src/awesome.rs");
     }
 }
